dc032df3f11a34f0d9c523df836fc6460a5895ab
Blake Rose <bdecktor@gmail.com>
2019-05-26 20:53:18 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
         :age => "17",
         :attitude => "worried"
       },
-      {}
+      {
         :name => "Mercutio",
         :age => "18",
         :attitude => "hot-headed"
@@ -19,7 +19,14 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        
+      }
         #your hashes here!
       ]
    }
